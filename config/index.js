fa5f4658466dcbf6709c047a2a4d7dc226419f65
haritha-webdev <haritha.nov19@gmail.com>
2022-05-02 14:17:46 +0530

    first phase

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/members-list/',
     proxyTable: {},
 
     // Various Dev Server settings
